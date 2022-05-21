4c6000f755948b0c0512b5a62fc4539af107b6a4
doorisclosed <polikarpov-sereg@mail.ru>
2022-05-21 22:38:43 +0300

    feat: done write-task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,17 +1,25 @@
 const fs = require('fs');
-const output = fs.createWriteStream('text.txt');
-
+const path = require('path');
 const { stdin, stdout } = process;
 
+fs.writeFile(path.join(__dirname, 'text.txt'), '', (err) => {
+  if (err) throw err;
+});
 
+stdout.write('Hello! Please, enter any text to write. Enter "exit" or Ctrl + С to finish\n');
 
+stdin.on('data', data => {
+  if (data.toString().toLocaleLowerCase().replace(/\s/g, '') === 'exit') {
+    stdout.write('Text saved, goodbye!'); 
+    process.exit();
+  }
 
+  fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
+    if (err) throw err;
+  });
+});
 
-
-
-
-
-
-
-
-
+process.on('SIGINT', () => {
+  stdout.write('Text saved, goodbye!'); 
+  process.exit();
+});
