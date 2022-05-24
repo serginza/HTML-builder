a78d194d9bd641ce526ca2f57e84e27d88a36089
doorisclosed <polikarpov-sereg@mail.ru>
2022-05-24 19:53:43 +0300

    feat: done copy-derectory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+  if (err) throw err;
+  
+  fs.readdir(path.join(__dirname, 'files-copy'), (_err, copyFiles) => {
+
+    for(let i = 0; i < copyFiles.length; i++) {
+      fs.unlink(path.join(__dirname, 'files-copy', copyFiles[i]), err => {
+        if(err) throw err;
+      });
+    }
+  });
+
+  fs.readdir(path.join(__dirname, 'files'), (_err, originalFiles) => {
+
+    for (let j = 0; j < originalFiles.length; j++) {
+      fs.copyFile(path.join(__dirname, 'files', originalFiles[j]), path.join(__dirname, 'files-copy', originalFiles[j]), err => {
+        if(err) throw err;
+      });
+    }
+  });
+});
